9cdd3a5aa791bd716e53340b3d952383e53ddcdc
farahinani <farahinaniahmad@gmail.com>
2025-12-05 15:24:30 +0800

    implement Testimonial section and add card component

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,6 +7,7 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -60,13 +61,14 @@ export const ProjectsSection = () => {
         {/* card wrapper */}
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20">
+            <Card 
+              key={project.title}
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
 
               {/* bg grain image */}
-              <div className="absolute inset-0 -z-10 opacity-5" style={{
+              {/* <div className="absolute inset-0 -z-10 opacity-5" style={{
                 backgroundImage: `url(${grainImage.src})`
-              }}></div>
+              }}></div> */}
 
               {/* make column for large screen */}
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
@@ -102,7 +104,7 @@ export const ProjectsSection = () => {
                   <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,6 +7,7 @@ import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 import grainImage from "@/assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 
 const testimonials = [
   {
@@ -43,27 +44,39 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           header="Happy Clients"
           title="What Clients Say about Me"
-          description="Don't just take my word for it. See what clients have to say about my work."
+          description="Don't just take my word for it. See what my clients have to say about my work."
         />
 
-        <div>
-          {testimonials.map((testimonial) => (
-            <div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6 relative overflow-hidden z-0">
-              <div className="absolute inset-0 opacity-5 -z-10"
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              // <div key={testimonial.name} className="bg-gray-800 rounded-3xl p-6 relative overflow-hidden z-0"
+              // >
+
+              <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
+                {/* <div className="absolute inset-0 opacity-5 -z-10"
                 style={{
                   backgroundImage: `url(${grainImage.src})`
-                }}></div>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </div>
-          ))}
+                }}>
+              </div> */}
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">{testimonial.position}</div>
+                  </div>
+                </div>
+                <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
