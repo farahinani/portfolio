1cc09f44ccd8bd14febf7a031b9235b8c44fe6ba
farahinani <farahinaniahmad@gmail.com>
2025-12-08 17:47:43 +0800

    Implement about section and add cardHeader comp, TechIcon com, toolboxItems comp

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,131 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import { CardHeader } from "@/components/CardHeader";
+import { ToolboxItems } from "@/components/ToolboxItems";
+
+const toolboxItems = [
+  {
+    title: 'Javascript',
+    iconType: JavascriptIcon
+  },
+  {
+    title: 'HTML5',
+    iconType: HTMLIcon
+  },
+  {
+    title: 'CSS3',
+    iconType: CssIcon
+  },
+  {
+    title: 'React',
+    iconType: ReactIcon
+  },
+  {
+    title: 'Chrome',
+    iconType: ChromeIcon
+  },
+  {
+    title: 'Github',
+    iconType: GithubIcon
+  },
+];
+
+const hobbies = [
+  {
+    title: 'Painting',
+    emoji: '🎨'
+  },
+  {
+    title: 'Photography',
+    emoji: '📷'
+  },
+  {
+    title: 'Gaming',
+    emoji: '🎮'
+  },
+  {
+    title: 'Hiking',
+    emoji: '🥾'
+  },
+  {
+    title: 'Music',
+    emoji: '🎵'
+  },
+  {
+    title: 'Fitness',
+    emoji: '🏋️'
+  },
+  {
+    title: 'Reading',
+    emoji: '📚'
+  }
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+
+        <SectionHeader
+          header="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, what I do, and what inspires me."
+        />
+
+        <div className="mt-20">
+          {/* ----- 1st card ----- */}
+          <Card className="h-[320px]">
+            {/* title */}
+            <CardHeader title="My Reads" description="Explore the books shaping my perspectives."/>
+            {/* image */}
+            <div className="w-40 mx-auto mt-8">
+              <Image src={bookImage} alt="Book cover" />
+            </div>
+          </Card>
+
+          {/* ----- 2nd card [toolbox]----- */}
+          <Card className="h-[320px] p-0">
+            {/* title */}
+             <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences" className="px-6 pt-6"/>
+            {/* image icon*/}
+            <ToolboxItems items={toolboxItems} className="mt-6"/>
+            <ToolboxItems items={toolboxItems} className="mt-6" />
+          </Card>
+
+          {/* ----- 3rd card [Hobbies] ----- */}
+          <Card>
+            {/* title */}
+            <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm"/>
+            {/* icons*/}
+            <div>
+              {hobbies.map(hobby => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+
+          {/* ----- 4th card [map image]----- */}
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="smile memoji" />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
