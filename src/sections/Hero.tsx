69a56e54dafe22ff3fe1c75637718867e4452760
farahinani <farahinaniahmad@gmail.com>
2025-12-19 04:49:43 +0800

    update deploy and hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -85,7 +85,7 @@ export const HeroSection = () => {
             </div>
 
             <div className="text-sm font-medium">
-              Available for new projects test version37
+              Available for new projects
             </div>
           </div>
 
