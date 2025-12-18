427a5d3128a7926f4213520e3df57a4ffedea141
farahinani <farahinaniahmad@gmail.com>
2025-12-19 07:06:19 +0800

    update deploy yml and hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -85,7 +85,7 @@ export const HeroSection = () => {
             </div>
 
             <div className="text-sm font-medium">
-              Available for new projects
+              Available for new projects hardcode secret
             </div>
           </div>
 
