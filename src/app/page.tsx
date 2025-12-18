ffeed5aa3c48482beed9d4a1bfe2a174cf944800
farahinani <farahinaniahmad@gmail.com>
2025-12-19 04:07:55 +0800

    TEST DEPLOY MARKER

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,35 @@
 export const dynamic = 'force-static'
-import { AboutSection } from "@/sections/About";
-import { Header } from "@/sections/Header";
-import { HeroSection } from "@/sections/Hero";
-import { ProjectsSection } from "@/sections/Projects";
-import { TapeSection } from "@/sections/Tape";
-import { PersonalProject } from "@/sections/PersonalProjects";
-import { TestimonialsSection } from "@/sections/Testimonials";
-import { ContactSection } from "@/sections/Contact";
-import { Footer } from "@/sections/Footer";
+// import { AboutSection } from "@/sections/About";
+// import { Header } from "@/sections/Header";
+// import { HeroSection } from "@/sections/Hero";
+// import { ProjectsSection } from "@/sections/Projects";
+// import { TapeSection } from "@/sections/Tape";
+// import { PersonalProject } from "@/sections/PersonalProjects";
+// import { TestimonialsSection } from "@/sections/Testimonials";
+// import { ContactSection } from "@/sections/Contact";
+// import { Footer } from "@/sections/Footer";
 
+// export default function Home() {
+//   return (
+//     <div>
+//       <Header />
+//       <HeroSection />
+//       <ProjectsSection />
+//       <TapeSection />
+//       <PersonalProject />
+//       {/* <TestimonialsSection /> */}
+//       <AboutSection />
+//       <ContactSection />
+//       <Footer />
+
+//     </div>
+//   );
+// }
 export default function Home() {
   return (
     <div>
-      <Header />
-      <HeroSection />
-      <ProjectsSection />
-      <TapeSection />
-      <PersonalProject />
-      {/* <TestimonialsSection /> */}
-      <AboutSection />
-      <ContactSection />
-      <Footer />
-
-      {/* TEST MARKER */}
-      <p id="deploy-test">
-        DEPLOY TEST 19 DEC 2025
-      </p>
+      <h1>STATIC TEST</h1>
+      <p>DEPLOY TEST 19 DEC 2025</p>
     </div>
-  );
+  )
 }
