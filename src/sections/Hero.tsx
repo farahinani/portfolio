cd02c1d0d81f6c5093f462c14aaf71613336520c
farahinani <farahinaniahmad@gmail.com>
2025-12-18 17:12:47 +0800

    test lagi

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -85,7 +85,7 @@ export const HeroSection = () => {
             </div>
 
             <div className="text-sm font-medium">
-              Available for new projects test
+              Available for new projects testaaa
             </div>
           </div>
 
