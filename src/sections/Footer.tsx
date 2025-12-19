d266cfe892d8f4f501edc61088d87150c3e09fd3
farahinani <farahinaniahmad@gmail.com>
2025-12-19 08:30:08 +0800

    update footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,18 +1,6 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 
 const footerLinks = [
-  // {
-  //   title: 'YouTube',
-  //   href: '#',
-  // },
-  // {
-  //   title: 'Twitter',
-  //   href: '#',
-  // },
-  // {
-  //   title: 'Instagram',
-  //   href: '#',
-  // },
   {
     title: 'LinkedIn',
     href: 'https://www.linkedin.com/in/farahinani/',
@@ -25,7 +13,7 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className='relative -z-10 overflow-x-clip'>
+    <footer className='relative overflow-x-clip'>
       {/* gradient bg */}
       <div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
 
@@ -34,7 +22,7 @@ export const Footer = () => {
           <div className='text-white/40'>&copy; 2025. All rights reserved.</div>
           <nav className='flex flex-col md:flex-row items-center gap-8'>
             {footerLinks.map((link) => (
-              <a href={link.href} key={link.title} className='inline-flex items-center gap-1.5'>
+              <a target="_blank" href={link.href} key={link.title} className='inline-flex items-center gap-1.5'>
                 <span className='font-semibold'>{link.title}</span>
                 <ArrowUpRightIcon className="size-4"/>
               </a>
